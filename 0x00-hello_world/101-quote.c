1787c2ff48d3cc836980d85cfdbca89ef20399a6
lucky66 <luckychukwu66@gmail.com>
2023-07-13 16:05:29 -0700

    adding

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - main function
+ *
+ * Return: 1
+ * /
+
+int main(void)
+{
+	write(2, "and that price of art is useful\" - Dora Korpar, 2915-10-19", 59);
+	return (1);
+}
