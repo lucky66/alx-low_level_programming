50291bd6c48218ccba064f5d7b18f57596e05070
lucky66 <luckychukwu66@gmail.com>
2023-07-13 16:11:05 -0700

    add

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - main function
  *
  * Return: 1
- * /
-
+ */
 int main(void)
 {
-	write(2, "and that price of art is useful\" - Dora Korpar, 2915-10-19", 59);
+	write(2, "and that price of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
