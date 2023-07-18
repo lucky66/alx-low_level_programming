0eff4508f88caf09bc76019f4a9f3d8f4f5dbb5e
lucky66 <luckychukwu66@gmail.com>
2023-07-18 07:29:53 -0700

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - main function
+ *
+ * Return: returns void
+ */
+void print_alphabet(void)
+{
+	char z;
+
+	for (z = 'a'; z <= 'z'; z++)
+	{
+		_putchar(z);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ * @n: the number
+ * Return: returns the last digit
+ */
+int print_last_digit(int n)
+{
+	int ld;
+
+	ld = n % 10;
+	if (ld < 0)
+	{
+		_putchar(-ld + 48);
+		return (-ld);
+	}
+	else
+	{
+		_putchar(ld + 48);
+		return (ld);
+	}
+}
