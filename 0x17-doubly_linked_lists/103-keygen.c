9d9c647d1e359a2e75a5cbc5d6aafdd832458684
lucky66 <luckychukwu66@gmail.com>
2023-10-13 14:46:46 -0700

    linked list

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,9 +3,9 @@
 #include <string.h>
 
 /**
- * main - Generates and prints Passwords for the crackme5 executable..
- * @argc: The Number of arguments supplied to the Program..
- * @argv: An array of pointers to the arguments..
+ * main - Generates and prints Passwords for the crackme5 executable.
+ * @argc: The Number of arguments supplied to the Program.
+ * @argv: An array of pointers to the arguments.
  *
  * Return: Always 0.
  */
@@ -39,4 +39,15 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	password[3] = codex[rand() & 63];
 
 	Tmp = 0;
+	for (i = 0; i < len; i++)
+		Tmp += (argv[1][i] * argv[1][i]);
+	password[4] = codex[(Tmp ^ 239) & 63];
+
+	for (i = 0; i < argv[1][0]; i++)
+		Tmp = rand();
+	password[5] = codex[(Tmp ^ 229) & 63];
+
+	password[6] = '\0';
+	printf("%s", password);
+	return (0);
 
